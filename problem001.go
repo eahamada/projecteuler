3f15fe27c87af1cd887b2ca5c6e0b0252d844edf
eahamada <elarif.ahamada@gmail.com>
2016-07-29 09:34:51 +0200

    projecteuler problem1
    https://projecteuler.net/problem=1

---
+++ problem001.go
@@ -0,0 +1,24 @@
+import "fmt"
+
+func isMult(a, b int) bool {
+	return a%b == 0
+}
+
+func _euler1(n, acc int) int {
+	if n == 0 {
+		return acc
+	} else {
+		if isMult(n, 3) || isMult(n, 5) {
+			acc += n
+		}
+		return _euler1(n-1, acc)
+	}
+}
+
+func euler1(n int) int {
+	return _euler1(n-1, 0)
+}
+
+func main() {
+	fmt.Println(euler1(1000))
+}
